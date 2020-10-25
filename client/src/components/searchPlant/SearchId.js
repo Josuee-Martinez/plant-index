0c985a2dcbf20afc9b9c8dfc2b061ff37a9c975f
Josue Martinez <josuemartinez3141@gmail.com>
2020-10-25 01:45:06 -0400

    plant info

--- client/src/components/searchPlant/SearchId.js
+++ client/src/components/searchPlant/SearchId.js
@@ -12,21 +12,48 @@ const SearchId = ({ match, searchPlantById, data: { plant } }) => {
         ""
       ) : (
         <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="card card-border mt-5">
-                <img src={plant.image_url} className="card-img-top" alt="..." />
-                <div className="card-body center">
-                  <h5>{plant.scientific_name}</h5>
-                  <p>
-                    Also know as "{plant.common_name}" is a plant of the{" "}
-                    {plant.family.name} family of genus {plant.genus.name}
-                  </p>
-                </div>
-              </div>
+          {/* <div className="row">
+            <div className="col-12"> */}
+          <div className="card card-border mt-5">
+            <img src={plant.image_url} className="card-img-top" alt="..." />
+            <div className="center p-4">
+              <h5>{plant.common_name}</h5>
+              <p>
+                Also know as "{plant.scientific_name}" is a plant of the{" "}
+                {plant.family.name} family of genus {plant.genus.name}
+              </p>
             </div>
+            <ul className="list-group">
+              <li className="list-group-item">
+                Bibliography{" "}
+                <span className="right-align">{plant.bibliography}</span>
+              </li>
+              <li className="list-group-item">
+                Family{" "}
+                <span className="right-align">{plant.family.common_name}</span>
+              </li>
+              <li className="list-group-item">
+                Genus <span className="right-align">{plant.genus.name}</span>
+              </li>
+              <li className="list-group-item">
+                Scientific Name
+                <span className="right-align">{plant.scientific_name}</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card card-border mt-4 p-4">
+            <div className="center">
+              <h5>Other sources</h5>
+            </div>
+            {plant.sources.map((item, i) => (
+              <p key={i}>
+                <a href={item.url}>{item.url}</a>
+              </p>
+            ))}
           </div>
         </div>
+        //   </div>
+        // </div>
       )}
     </div>
   );
