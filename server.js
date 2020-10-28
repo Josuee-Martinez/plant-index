d5e3dc6ff5e458c37ffdc81f8bb5e9965dafe074
Josue Martinez <josuemartinez3141@gmail.com>
2020-10-28 00:19:59 -0400

    deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -9,11 +10,19 @@ app.use(require("./middleware/headers"));
 
 app.use("/api/search", searchPlant);
 
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+
+   app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+   );
+}
+
 const server = app.listen(5000, console.log("express running"));
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+   console.log(`Error: ${err.message}`);
 
-  server.close(() => process.exit(1));
+   server.close(() => process.exit(1));
 });
